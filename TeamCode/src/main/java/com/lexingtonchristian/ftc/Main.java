2c09fa544817aa4cc6b01b241dc32164f4583727
ScienceMind11 <datac27@365.lexca.org>
2025-09-09 11:20:03 -0400

    Update OpMode to make use of new Drive and Side classes for testing purposes

--- TeamCode/src/main/java/com/lexingtonchristian/ftc/Main.java
+++ TeamCode/src/main/java/com/lexingtonchristian/ftc/Main.java
@@ -1,5 +1,6 @@
 package com.lexingtonchristian.ftc;
 
+import com.lexingtonchristian.ftc.motor.Drive;
 import com.lexingtonchristian.ftc.motor.DriveMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,23 +10,37 @@ import com.qualcomm.robotcore.hardware.IMU;
 @TeleOp
 public class Main extends LinearOpMode {
 
-    private DriveMotor leftMotor;
-    private DriveMotor rightMotor;
+    private DriveMotor backLeft;
+    private DriveMotor frontLeft;
+    private DriveMotor backRight;
+    private DriveMotor frontRight;
+
+    private Drive.Side left;
+    private Drive.Side right;
+    private Drive drive;
 
     private IMU imu;
 
     @Override
     public void runOpMode() {
 
-        leftMotor = new DriveMotor(hardwareMap.get(DcMotor.class, "left_motor"), 3.0);
-        rightMotor = new DriveMotor(hardwareMap.get(DcMotor.class, "right_motor"), 3.0);
+        double wheelDiameter = 3.0;
+
+        this.backLeft = new DriveMotor(hardwareMap.get(DcMotor.class, "back_left"), wheelDiameter);
+        this.frontLeft = new DriveMotor(hardwareMap.get(DcMotor.class, "front_left"), wheelDiameter);
+        this.backRight = new DriveMotor(hardwareMap.get(DcMotor.class, "back_right"), wheelDiameter);
+        this.frontRight = new DriveMotor(hardwareMap.get(DcMotor.class, "front_right"), wheelDiameter);
+
+        this.left = new Drive.Side(this.backLeft, this.frontLeft);
+        this.right = new Drive.Side(this.backRight, this.frontRight);
+
+        this.drive = new Drive(this.left, this.left, 100.0);
 
         imu = hardwareMap.get(IMU.class, "imu");
 
         waitForStart();
 
-        leftMotor.rotate(90.0);
-        rightMotor.rotate(90.0);
+        this.drive.turnStationary(90.0);
 
     }
 
