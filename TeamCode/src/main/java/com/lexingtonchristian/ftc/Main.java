c8b7e8efbf14711b81628dd595e4c83a21594cfc
keawi26 <keawi26@365.lexca.org>
2025-09-17 12:56:57 -0400

    error with previous commit

--- TeamCode/src/main/java/com/lexingtonchristian/ftc/Main.java
+++ TeamCode/src/main/java/com/lexingtonchristian/ftc/Main.java
@@ -25,20 +25,16 @@ public class Main extends LinearOpMode {
 
         waitForStart();
 
-        while (this.opModeIsActive()) {
 
-            double mult = this.gamepad1.right_bumper ? 0.2 : 1.0;
 
-            double stickY = this.gamepad1.left_stick_y;
-            double stickX = this.gamepad1.left_stick_x;
+        double stickY = this.gamepad1.left_stick_y;
+        double stickX = this.gamepad1.left_stick_x;
 
-            double leftPower = clamp(stickX - stickY, -1.0, 1.0);
-            double rightPower = clamp(stickX + stickY, -1.0, 1.0);
+        double leftPower = clamp(stickX - stickY, -1.0, 1.0);
+        double rightPower = clamp(stickX + stickY, -1.0, 1.0);
 
-            this.backRight.setPower(rightPower * mult);
-            this.backLeft.setPower(leftPower * mult);
-
-        }
+        this.backRight.setPower(rightPower);
+        this.backLeft.setPower(leftPower);
 
     }
 
