8a70b2ee1d60131a9da27413158e897a95f8708b
datac27 <datac27@365.lexca.org>
2025-09-19 16:49:47 -0400

    fix hub imu orientation errors

--- TeamCode/src/main/java/org/lexingtonchristian/ftc/lib/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/lib/drive/DriveConstants.java
@@ -1,6 +1,7 @@
 package org.lexingtonchristian.ftc.lib.drive;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 /*
@@ -18,6 +19,9 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
  */
 @Config
 public class DriveConstants {
+
+    public static final RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    public static final RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
     /*
      * These are motor constants that should be listed online for your motors.
