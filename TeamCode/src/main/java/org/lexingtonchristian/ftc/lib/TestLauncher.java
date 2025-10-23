f719e39e2e21b333f70c3d81a33e7bb931b5f080
keawi26 <keawi26@365.lexca.org>
2025-10-23 17:09:51 -0400

    updated TestLauncher.java

--- TeamCode/src/main/java/org/lexingtonchristian/ftc/lib/TestLauncher.java
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/lib/TestLauncher.java
@@ -6,24 +6,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class TestLauncher extends LinearOpMode {
     private DcMotor launcherLeft;
     private DcMotor launcherRight;
+    private Servo launcherServo;
 
     @Override
     public void runOpMode() {
 
         this.launcherLeft = hardwareMap.get(DcMotor.class, "launcherLeft");
         this.launcherRight = hardwareMap.get(DcMotor.class, "launcherRight");
+        this.launcherServo = hardwareMap.get(Servo.class, "launcherServo");
 
         this.launcherLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
-        launcherLeft.setPower(this.gamepad1.right_trigger);
-        launcherRight.setPower(this.gamepad1.right_trigger);
+        launcherLeft.setPower(0.5);
+        launcherRight.setPower(0.5);
 
+        Servo.Direction launch = this.gamepad1.x ? Servo.Direction.FORWARD : null;
+
+        launcherServo.setDirection(launch);
     }
 }
