b79d40afdb118e76835ae4f2c2eeac2e5ea460e4
datac27 <datac27@365.lexca.org>
2025-10-24 17:27:24 -0400

    fix directions/rotation

--- TeamCode/src/main/java/org/lexingtonchristian/ftc/PrimaryTeleOp.java
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/PrimaryTeleOp.java
@@ -21,10 +21,10 @@ import java.util.stream.Collectors;
 
 /**
  *
- * {@code backRight} - {@code Port 0}, {@code Control Hub} <br/>
- * {@code backLeft}  - {@code Port 0}, {@code Control Hub}
- * {@code backRight} - {@code Port 0}, {@code Control Hub}
- * {@code backRight} - {@code Port 0}, {@code Control Hub}
+ * {@code backRight}  - {@code Port 0}, {@code Control Hub} <br/>
+ * {@code backLeft}   - {@code Port 1}, {@code Control Hub} <br/>
+ * {@code frontRight} - {@code Port 2}, {@code Control Hub} <br/>
+ * {@code frontLeft}  - {@code Port 3}, {@code Control Hub}
  *
  */
 
@@ -60,11 +60,11 @@ public class PrimaryTeleOp extends LinearOpMode {
             double lY = this.gamepad1.left_stick_y;
             double rX = this.gamepad1.right_stick_x;
 
-            this.backLeft.setPower(left(lX + lY, rX) * mult);
-            this.frontLeft.setPower(left(lX - lY, rX) * mult);
+            this.backLeft.setPower(left(lY + lX, rX) * mult);
+            this.frontLeft.setPower(left(lY - lX, rX) * mult);
 
-            this.backRight.setPower(right(lX + lY, rX) * mult);
-            this.frontRight.setPower(right(lX - lY, rX) * mult);
+            this.backRight.setPower(right(lY - lX, rX) * mult);
+            this.frontRight.setPower(right(lY + lX, rX) * mult);
 
             this.launcherLeft.setPower(power);
             this.launcherRight.setPower(power);
@@ -89,7 +89,11 @@ public class PrimaryTeleOp extends LinearOpMode {
                 }
             }
 
-            if (this.gamepad1.b) this.launcherServo.setPower(0.7);
+            if (this.gamepad1.b) {
+                this.launcherServo.setPower(0.7);
+            } else {
+                this.launcherServo.setPower(0.0);
+            }
 
         }
 
@@ -130,6 +134,10 @@ public class PrimaryTeleOp extends LinearOpMode {
         this.frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
 
         this.launcherLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.launcherServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        this.frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.tagProcessor = AprilTagProcessor.easyCreateWithDefaults();
         this.webcam = hardwareMap.get(WebcamName.class, "webcam");
@@ -137,7 +145,7 @@ public class PrimaryTeleOp extends LinearOpMode {
                 .setCamera(webcam)
                 .addProcessor(tagProcessor)
                 .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
-                .setCameraResolution(new Size(680, 420))
+                .setCameraResolution(new Size(640, 360))
                 .build();
 
     }
