c258e27e08916e3c8f3262e87e8539852425d93a
datac27 <datac27@365.lexca.org>
2025-11-21 16:27:22 -0500

    new test autos

---
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/Second10Auto.java
@@ -0,0 +1,48 @@
+package org.lexingtonchristian.ftc;
+
+import static org.lexingtonchristian.ftc.lib.drive.DriveConstants.TRACK_WIDTH;
+import static org.lexingtonchristian.ftc.lib.drive.DriveConstants.WHEELBASE;
+import static org.lexingtonchristian.ftc.lib.drive.DriveConstants.kA;
+import static org.lexingtonchristian.ftc.lib.drive.DriveConstants.kStatic;
+import static org.lexingtonchristian.ftc.lib.drive.DriveConstants.kV;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.lexingtonchristian.ftc.util.Constants;
+import org.lexingtonchristian.ftc.util.Launcher;
+import org.lexingtonchristian.ftc.util.RRMecanum;
+import org.lexingtonchristian.ftc.util.TagDetector;
+
+@Autonomous(name = "10 Seconds", group = "Test")
+public class Second10Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        Launcher launcher = Constants.initLauncher(hardwareMap);
+        TagDetector tagDetector = new TagDetector(hardwareMap.get(WebcamName.class, "webcam"));
+        RRMecanum drive = new RRMecanum(kV, kA, kStatic, TRACK_WIDTH, WHEELBASE, 1.0, hardwareMap) {
+            @Override
+            protected double getRawExternalHeading() {
+                return 0;
+            }
+        };
+
+        waitForStart();
+
+        drive.setDrivePower(move(0.0, 0.55, 0.0));
+        this.sleep(10000);
+        drive.zero();
+
+    }
+
+
+    public static Pose2d move(double x, double y, double heading) {
+        return new Pose2d(x, y, heading);
+    }
+
+
+}
