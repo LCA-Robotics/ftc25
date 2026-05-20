d7812d7d4396af98bcfa1651e402d35343c0645f
datac27 <datac27@365.lexca.org>
2026-05-20 10:43:52 -0400

    fieldcentric motion

--- TeamCode/src/main/java/org/lexingtonchristian/ftc/op/tele/PrimaryTeleOp.java
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/op/tele/PrimaryTeleOp.java
@@ -1,38 +1,93 @@
 package org.lexingtonchristian.ftc.op.tele;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.lexingtonchristian.ftc.util.MathHelper;
+
+import static java.lang.Math.*;
 
 @TeleOp(name = "Primary", group = "Competition")
 public class PrimaryTeleOp extends OpMode {
 
-    private Gamepad currentGamepad;
-    private Gamepad previousGamepad;
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor frontRight;
+
+    private IMU imu;
+
+    private Gamepad currentGamepad = new Gamepad();
+    private Gamepad previousGamepad = new Gamepad();
+
+    private boolean fieldCentric = false;
 
     @Override
     public void init() {
 
-        this.currentGamepad.copy(this.gamepad1);
+        this.backLeft = this.hardwareMap.get(DcMotor.class, "backLeft");
+        this.frontLeft = this.hardwareMap.get(DcMotor.class, "frontLeft");
+        this.backRight = this.hardwareMap.get(DcMotor.class, "backRight");
+        this.frontRight = this.hardwareMap.get(DcMotor.class, "frontRight");
+
+        this.backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        imu = this.hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
+        )));
+
+        currentGamepad.copy(this.gamepad1);
 
     }
 
     @Override
     public void loop() {
 
-        this.previousGamepad.copy(this.currentGamepad);
-        this.currentGamepad.copy(this.gamepad1);
+        previousGamepad.copy(this.currentGamepad);
+        currentGamepad.copy(this.gamepad1);
 
-        handleState();
+        if (currentGamepad.options && !previousGamepad.options) imu.resetYaw();
+        if (currentGamepad.back && !previousGamepad.back) fieldCentric = !fieldCentric;
 
+        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
+        double x0 = currentGamepad.left_stick_x * -1;
+        double y0 = currentGamepad.left_stick_y * -1;
+        double r0 = currentGamepad.right_stick_x;
 
+        double x1 = x0;
+        double y1 = y0;
+        if (fieldCentric) {
+            x1 = ( x0 * cos(heading) ) - ( y0 * sin(heading) );
+            y1 = ( x0 * sin(heading) ) + ( y0 * cos(heading) );
+        }
 
-    }
+        double pBackLeft   = x1 - y1 - r0;
+        double pFrontLeft  = x1 + y1 - r0;
+        double pBackRight  = x1 + y1 + r0;
+        double pFrontRight = x1 - y1 + r0;
 
-    public void handleState() {
+        double max = MathHelper.max(pBackLeft, pFrontLeft, pBackRight, pFrontRight);
+        if (max > 1.0) {
+            pBackLeft   /= max;
+            pFrontLeft  /= max;
+            pBackRight  /= max;
+            pFrontRight /= max;
+        }
 
-
+        backLeft.setPower(pBackLeft);
+        frontLeft.setPower(pFrontLeft);
+        backRight.setPower(pBackRight);
+        frontRight.setPower(pFrontRight);
 
     }
 
--- TeamCode/src/main/java/org/lexingtonchristian/ftc/util/Constants.java
+++ TeamCode/src/main/java/org/lexingtonchristian/ftc/util/Constants.java
@@ -6,10 +6,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.lexingtonchristian.ftc.components.TagDetector;
 
-/**
- * Lists constants for use across several classes in order to simplify code and reduce use of magic
- * constants.
- */
 public class Constants {
 
     public static final double WHEEL_RADIUS = 2.0;
@@ -20,12 +16,6 @@ public class Constants {
     public static final double GEAR_RATIO = 1;
 
     public static final double TICKS_PER_REV = 537.6;
-    public static final double MAX_RPM = 312;
-
-    public static final double DRIVETRAIN_P    = 1.50;
-    public static final double DRIVETRAIN_I    = 0.00;
-    public static final double DRIVETRAIN_D    = 0.00;
-    public static final double DRIVETRAIN_F    = 19.5;
 
     public static TagDetector initDetector(HardwareMap map) {
         return new TagDetector(map.get(WebcamName.class, "webcam"));
